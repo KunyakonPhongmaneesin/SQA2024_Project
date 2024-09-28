d6b4beae20f74a1f1eb345c22a936e9db5a9d6ee
Sunjiruntn <siwakul.s@kkumail.com>
2024-09-28 18:23:40 +0700

    ADD

--- Java/Adapter/R1/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Adaptee adaptee = new Adaptee();
-        Target target = new Adapter(adaptee);
-        target.request();
-    }
-}
